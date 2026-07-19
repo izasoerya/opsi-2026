677d9f3fae6971b0fe0bb41c187ce00bfd44d1aa
izasoerya <ihza.surya@gmail.com>
2026-07-20 01:49:14 +0700

    chore: refactor bh1750 dependency

--- include/sensor/configs/bh1750_sensor.h
+++ include/sensor/configs/bh1750_sensor.h
@@ -1,7 +1,7 @@
 #if !defined(BH1750_SENSOR_H)
 #define BH1750_SENSOR_H
 
-#include <BH1750.h>
+#include <ArtronShop_BH1750.h>
 #include "../base_sensor.h"
 
 class BH1750Sensor : public BaseSensor
@@ -9,31 +9,26 @@ class BH1750Sensor : public BaseSensor
 private:
     TwoWire *_wire;
     uint8_t _address;
-    BH1750 _lux;
+    ArtronShop_BH1750 _lux;
 
 public:
     BH1750Sensor(
         unsigned char id, const char *name,
         TwoWire *wire, uint8_t address)
-        : BaseSensor(id, name), _wire(wire), _address(address) {}
+        : BaseSensor(id, name), _wire(wire), _address(address), _lux(ArtronShop_BH1750(_address, _wire)) {}
 
     ~BH1750Sensor() override = default;
 
     bool begin()
     {
-        if (!_lux.begin(_address))
+        if (!_lux.begin())
             return false;
-        _lux.powerOn();
-        _lux.setMode(BH1750_CONTINOUS_H_RES_MOD);
         return true;
     }
 
     float read() override
     {
-        if (_lux.isMeasurementReady())
-        {
-            return _lux.readValue();
-        }
+        return _lux.light();
     }
 };
 
@@ -42,13 +37,13 @@ class MockBH1750Sensor : public BaseSensor
 private:
     TwoWire *_wire;
     uint8_t _address;
-    BH1750 _lux;
+    ArtronShop_BH1750 _lux;
 
 public:
     MockBH1750Sensor(
         unsigned char id, const char *name,
         TwoWire *wire, uint8_t address)
-        : BaseSensor(id, name), _wire(wire), _address(address) {}
+        : BaseSensor(id, name), _wire(wire), _address(address), _lux(ArtronShop_BH1750(_address, _wire)) {}
 
     ~MockBH1750Sensor() override = default;
 

7e1b956f3eaeb5b918804bf4f6b83cc073f1fe1d
izasoerya <ihza.surya@gmail.com>
2026-07-30 01:45:01 +0700

    feat: to json string method

--- src/projects/brin-bandung/models.h
+++ src/projects/brin-bandung/models.h
@@ -2,13 +2,15 @@
 #define MODELS_H
 
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include "../utils/parser.h"
 
-struct BandungPersemaianEntity
+struct SensorDto
 {
+    const uint32_t deviceId;
     const float rainFall;
     const float windSpeed;
-    const char *windDirection;
+    const WindDirectionEnum windDirection;
     const float airTemperature;
     const float airHumidity;
     const char timestamp[9];
@@ -18,13 +20,27 @@ struct BandungPersemaianEntity
         static char buffer[128];
         snprintf(buffer, sizeof(buffer),
                  "rain_fall: %.1f\nwind_direction: %s\nwind_speed: %.2f\nair_temperature: %.1f\nair_humidity: %.1f\ntimestamp: %s",
-                 rainFall, windSpeed, windDirection, airTemperature, airHumidity, timestamp);
+                 rainFall, Parser::parseWindDirection(windDirection), windSpeed, airTemperature, airHumidity, timestamp);
         return buffer;
+    }
+
+    size_t toJson(char *out, size_t outSize) const
+    {
+        JsonDocument doc;
+        doc["device_id"] = deviceId;
+        doc["rain_fall"] = rainFall;
+        doc["wind_direction"] = Parser::parseWindDirection(windDirection);
+        doc["wind_speed"] = windSpeed;
+        doc["temperature"] = airTemperature;
+        doc["humidity"] = airHumidity;
+
+        return serializeJsonPretty(doc, out, outSize);
     }
 };
 
-struct BandungPersemaianSystemEntity
+struct SystemLogDto
 {
+    uint32_t deviceId;
     uint32_t freeHeap;
     uint32_t largestFreeBlock;
     uint32_t minFreeHeap;
@@ -37,6 +53,18 @@ struct BandungPersemaianSystemEntity
                  "freeHeap: %lu\nfreeBlock: %lu\nminFreeHeap: %lu\nResetReason: %s",
                  freeHeap, largestFreeBlock, minFreeHeap, Parser::parseResetReasonESP(lastResetReason));
         return buffer;
+    }
+
+    size_t toJson(char *out, size_t outSize)
+    {
+        JsonDocument doc;
+        doc["device_id"] = deviceId;
+        doc["free_heap"] = freeHeap;
+        doc["largest_free_block"] = largestFreeBlock;
+        doc["min_free_heap"] = minFreeHeap;
+        doc["reset_reason"] = Parser::parseResetReasonESP(lastResetReason);
+
+        return serializeJsonPretty(doc, out, outSize);
     }
 };
 

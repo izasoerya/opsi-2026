76f2d24f0c19b3fc52d2b111f5498d7d292beefc
izasoerya <ihza.surya@gmail.com>
2026-07-31 00:50:11 +0700

    feat: calibration for aquaponic project

--- include/sensor/configs/ph_dfrobot_sensor.h
+++ include/sensor/configs/ph_dfrobot_sensor.h
@@ -19,31 +19,30 @@ private:
     uint8_t _channelADS = 255;
     uint8_t _pinAnalog = 255;
 
-    static const uint8_t _windowSize = 40;
-    uint16_t _rawBuffer[_windowSize];
-    uint8_t _arrayIndex = 0;
-    uint64_t _prevSampling = 0;
-
     const float _offset = 0;
-    const float _vref = 3.3;
-    const uint16_t _adcResolution = 4095;
+    const float _vref = 4.096;
+    const uint16_t _adcResolution = 32768;
     ADS1115Module *_ads = nullptr;
 
     BaseFilter *_filter = nullptr;
     BaseSensor *_sensor = nullptr;
 
+    uint64_t _prevSampling = 0;
+
 public:
     PHDFRobotSensor(
         unsigned char id, const char *name,
         uint8_t channelADS, ADS1115Module *ads,
-        BaseFilter *_filter = nullptr, BaseSensor *_sensor = nullptr)
-        : BaseSensor(id, name), _channelADS(channelADS), _ads(ads) {}
+        BaseFilter *filter = nullptr, BaseSensor *sensor = nullptr)
+        : BaseSensor(id, name), _channelADS(channelADS), _ads(ads),
+          _filter(filter), _sensor(sensor) {}
 
     PHDFRobotSensor(
         unsigned char id, const char *name,
         uint8_t pinAnalog,
-        BaseFilter *_filter = nullptr, BaseSensor *_sensor = nullptr)
-        : BaseSensor(id, name), _pinAnalog(pinAnalog) {}
+        BaseFilter *filter = nullptr, BaseSensor *sensor = nullptr)
+        : BaseSensor(id, name), _pinAnalog(pinAnalog),
+          _filter(filter), _sensor(sensor) {}
 
     ~PHDFRobotSensor() override = default;
 
@@ -65,15 +64,30 @@ public:
         }
     }
 
+    float readVoltage()
+    {
+        float val = _ads != nullptr ? (float)_ads->read(_channelADS) : (float)analogRead(_pinAnalog);
+        if (_filter != nullptr)
+            _filter->filter(val);
+        float averageVoltage = val * (float)_vref * 1000.0f / _adcResolution;
+        return averageVoltage;
+    }
+
     float read() override
     {
         float val = _ads != nullptr ? (float)_ads->read(_channelADS) : (float)analogRead(_pinAnalog);
         if (_filter != nullptr)
             _filter->filter(val);
-        float averageVoltage = val * (float)_vref / _adcResolution;
+        float averageVoltage = val * (float)_vref * 1000.0f / _adcResolution; // mV
 
-        float pHValue = 3.5 * averageVoltage + _offset;
-        return pHValue;
+        //! MAKESHIFT SOLUTION, NEED TO ACTUALLY TEST PH
+        const float V_686 = 2071.0f; // measured mV at pH 6.86
+        const float V_401 = 2023.0f; // measured mV at pH 4.01
+
+        float slope = (4.01f - 6.86f) / (V_401 - V_686);
+        float intercept = 6.86f - slope * V_686;
+
+        return slope * averageVoltage + intercept;
     }
 };
 

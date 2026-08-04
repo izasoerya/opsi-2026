8a508da22d941657a8d5956e96537ae95768c5c7
izasoerya <ihza.surya@gmail.com>
2026-08-04 22:45:06 +0700

    feat: force insecure for lower overhead

--- lib/wifi_bundle/transport.cpp
+++ lib/wifi_bundle/transport.cpp
@@ -9,11 +9,13 @@ int SupabaseTransport::post(const char *endpoint,
                             const char *payload,
                             const char *contentType)
 {
+    WiFiClientSecure secureClient; // Use the secure variant for HTTPS
+    secureClient.setInsecure();
     HTTPClient http;
     char url[128];
     snprintf(url, sizeof(url), "%s/rest/v1/%s", _supabaseUrl, endpoint);
 
-    http.begin(url);
+    http.begin(secureClient, url);
     _setHeaders(http, contentType);
     int response = http.POST(payload);
     http.end();
@@ -28,8 +30,11 @@ int BasicHTTPTransport::post(const char *endpoint,
                              const char *payload,
                              const char *contentType)
 {
+    WiFiClientSecure secureClient; // Use the secure variant for HTTPS
+    secureClient.setInsecure();
+
     HTTPClient http;
-    http.begin(endpoint);
+    http.begin(secureClient, endpoint);
     http.addHeader("Content-Type", contentType);
     int response = http.POST(payload);
     http.end();

c5c9712f3c9505f5627e04314eddcfc8d7d292f4
izasoerya <ihza.surya@gmail.com>
2026-08-06 23:27:30 +0700

    feat: slave arr main program finished

---
+++ src/projects/brin-bandung/slave_arr.cpp
@@ -0,0 +1,203 @@
+#include <Arduino.h>
+#include <esp_task_wdt.h>
+#include <ModbusServerTCPasync.h>
+#include <ElegantOTA.h>
+#include <WebSerial.h>
+#include <Wire.h>
+#include <WireGuard-ESP32.h>
+#include <DFRobot_RainfallSensor.h>
+
+#include "config.h" // .env
+#include "transmitter/configs/wifi_module.h"
+#include "../utils/utils.h"
+
+const char *ssid = "NodeSensorWiFi1";
+const char *password = "muhammadnabiyullah";
+const char *hostname = "slave-arr-bandung-persemaian-1";
+WiFiModule wifi(ssid, password, hostname, WIFI_POWER_19_5dBm);
+
+AsyncWebServer server(80);
+WireGuard wg;
+
+/**
+ * @brief Modbus address register mapping
+ *  array[0] = (float) rainfall
+ *  array[1-2] = (uint32_t) free heap
+ *  array[3-4] = (uint32_t) largest block
+ *  array[5-6] = (uint32_t) min heap
+ *  array[7] = (uint8_t) reset reason
+ */
+ModbusServerTCPasync modbusServer;
+const uint8_t MAX_REGISTER = 4;
+uint16_t modbusData[MAX_REGISTER];
+ModbusMessage FC03(ModbusMessage request);
+ModbusMessage FC06(ModbusMessage request);
+
+DFRobot_RainfallSensor_I2C rainSensor(&Wire);
+
+const uint8_t pinSDA = 4; // TODO: CHANGE TO APPROPRIATE PIN
+const uint8_t pinSCL = 5; // TODO: CHANGE TO APPROPRIATE PIN
+
+const char *ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 25200;
+const int daylightOffset_sec = 0;
+
+uint32_t prevTimeReading = 0;
+uint32_t delayReading = 10000;
+bool shouldRestartNow = false;
+bool shouldResetRainfall = false;
+bool hasResetToday = false;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    if (wifi.begin())
+        Serial.println(wifi.localIP());
+
+    esp_task_wdt_init(60, true);
+    esp_task_wdt_add(NULL);
+
+    ElegantOTA.setAutoReboot(true);
+    ElegantOTA.begin(&server);
+    WebSerial.begin(&server);
+    server.begin();
+
+    Wire.begin(pinSDA, pinSCL);
+    if (rainSensor.begin())
+    {
+        Serial.println("Init sensor rainfall fail!");
+        WebSerial.println("Init sensor rainfall fail!");
+    }
+
+    uint8_t retryCounter = 0;
+    struct tm timeinfo;
+    configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org", "time.google.com");
+    while (!getLocalTime(&timeinfo) && retryCounter < 20)
+    {
+        Serial.print(".");
+        delay(500);
+        if (retryCounter >= 20)
+        {
+            Serial.println("\nNTP Sync Failed! Restarting...");
+            WebSerial.println("\nNTP Sync Failed! Restarting...");
+            ESP.restart(); // Critical: WireGuard handshake will fail without correct time
+        }
+        retryCounter++;
+    }
+
+    IPAddress wgLocalIP;
+    wgLocalIP.fromString(WG_DEVICE_SLAVE_ARR_LOCAL_IP_1);
+    Serial.printf("wg ip: %s", wgLocalIP.toString());
+    bool wgOk = wg.begin(wgLocalIP, WG_DEVICE_SLAVE_ARR_PRIVATE_KEY_1,
+                         WG_SERVER_PUBLIC_IP, WG_SERVER_PUBLIC_KEY, WG_ENDPOINT_PORT);
+    if (wgOk)
+    {
+        Serial.println("WireGuard successfully initialized on ESP32!");
+        WebSerial.println("WireGuard successfully initialized on ESP32!");
+    }
+    else
+    {
+        Serial.println("WireGuard initialization failed!");
+        WebSerial.println("WireGuard initialization failed!");
+    }
+
+    modbusServer.registerWorker(1, READ_HOLD_REGISTER, &FC03); // FC=03 for serverID=1
+    modbusServer.registerWorker(1, WRITE_HOLD_REGISTER, &FC06);
+    modbusServer.start(5000, 2, 20000);
+    modbusData[12] = delayReading;
+}
+
+void loop()
+{
+    esp_task_wdt_reset();
+
+    if (millis() - prevTimeReading > delayReading)
+    {
+        Serial.println("HEARTBEAT");
+        WebSerial.println("HEARTBEAT");
+        prevTimeReading = millis();
+
+        // === SENSOR LOG DATA ===
+        modbusData[0] = rainSensor.getRainfall(24); // return in mm/day
+
+        // === SYSTEM LOG DATA ===
+        uint32_t freeHeap = ESP.getFreeHeap();
+        modbusData[1] = (uint16_t)(freeHeap >> 16);    // High word
+        modbusData[2] = (uint16_t)(freeHeap & 0xFFFF); // Low word
+        uint32_t largestBlock = heap_caps_get_largest_free_block(MALLOC_CAP_DEFAULT);
+        modbusData[3] = (uint16_t)(largestBlock >> 16);
+        modbusData[4] = (uint16_t)(largestBlock & 0xFFFF);
+        uint32_t minHeap = ESP.getMinFreeHeap();
+        modbusData[5] = (uint16_t)(minHeap >> 16);
+        modbusData[6] = (uint16_t)(minHeap & 0xFFFF);
+        modbusData[7] = (uint16_t)esp_reset_reason();
+
+        struct tm timeinfo;
+        getLocalTime(&timeinfo);
+        int hour = timeinfo.tm_hour;
+        int minute = timeinfo.tm_min;
+    }
+}
+
+ModbusMessage FC03(ModbusMessage request)
+{
+    /**
+     * @brief Info about modbus TCP frame
+     * | Slave id | Function code | Start Add | Length Add |
+     * | 1 bytes  | 1 bytes       | 2 bytes   | 2 bytes    |
+     */
+    ModbusMessage response; // in
+    uint16_t addr = 0;      // start address
+    uint16_t words = 0;     // # of words requested
+    request.get(2, addr);   // since start address is on bytes 3 of modbus frame
+    request.get(4, words);  // since length address is on bytes 5 of modbus frame
+
+    if ((addr + words) > MAX_REGISTER)
+        response.setError(request.getServerID(), request.getFunctionCode(), ILLEGAL_DATA_ADDRESS);
+
+    response.add(request.getServerID(), request.getFunctionCode(), (uint8_t)(words * 2));
+    if (request.getFunctionCode() == READ_HOLD_REGISTER)
+    {
+        for (uint8_t i = 0; i < words; i++)
+            response.add((uint16_t)modbusData[addr + i]);
+
+        Serial.printf("Req Slave Id: %d, FC: %d, Data: [%d, %d, %d, %d, %d]\n",
+                      request.getServerID(), request.getFunctionCode(),
+                      modbusData[addr + 0], modbusData[addr + 1], modbusData[addr + 2], modbusData[addr + 3], modbusData[addr + 4]);
+
+        WebSerial.printf("Req Slave Id: %d, FC: %d, Data: [%d, %d, %d, %d, %d]\n",
+                         request.getServerID(), request.getFunctionCode(),
+                         modbusData[addr + 0], modbusData[addr + 1], modbusData[addr + 2], modbusData[addr + 3], modbusData[addr + 4]);
+    }
+    return response;
+}
+
+ModbusMessage FC06(ModbusMessage request)
+{
+    ModbusMessage response;
+    uint16_t addr = 0;  // Register address
+    uint16_t value = 0; // Value to write
+
+    request.get(2, addr);  // read address from request
+    request.get(4, value); // read value from request
+
+    Serial.printf("FC06: Write register %d = %d\n", addr, value);
+
+    // Address overflow check
+    if (addr >= 16)
+    { // Your modbusData array is 16 words
+        response.setError(request.getServerID(), request.getFunctionCode(), ILLEGAL_DATA_ADDRESS);
+        return response;
+    }
+
+    // Write to modbusDatary
+    modbusData[addr] = value;
+
+    // Echo back the request (standard FC06 response)
+    response.add(request.getServerID(), request.getFunctionCode());
+    response.add(addr);
+    response.add(value);
+
+    return response;
+}

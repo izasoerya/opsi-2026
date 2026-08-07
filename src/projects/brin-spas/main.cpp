7d087c487ccc23da505b8ff41229131801388592
Hanan <hanan@gmail.com>
2026-08-08 01:46:40 +0700

    feat: lcd feature if exist

--- src/projects/brin-spas/main.cpp
+++ src/projects/brin-spas/main.cpp
@@ -2,10 +2,11 @@
 #include <ElegantOTA.h>
 #include <WebSerial.h>
 #include <WireGuard-ESP32.h>
+#include <esp_task_wdt.h>
+#include <LiquidCrystal_I2C.h>
 
 #include "config.h" // .env
 #include "transmitter/configs/wifi_module.h"
-#include <esp_task_wdt.h>
 
 const char *ssid = "Subhanallah";
 const char *password = "muhammadnabiyullah";
@@ -18,9 +19,19 @@ const char *ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 25200;
 const int daylightOffset_sec = 0;
 
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+bool isLCDExist = false;
+
+bool isI2CDevicePresent(uint8_t address)
+{
+    Wire.beginTransmission(address);
+    return (Wire.endTransmission() == 0); // 0 = ACK received, device exists
+}
+
 void setup()
 {
     Serial.begin(115200);
+    Wire.begin();
 
     esp_task_wdt_init(60, true);
     esp_task_wdt_add(NULL);
@@ -37,6 +48,13 @@ void setup()
     ElegantOTA.begin(&server);
     WebSerial.begin(&server);
     server.begin();
+
+    if (isI2CDevicePresent(0x27))
+    {
+        lcd.init(); // initialize the lcd
+        lcd.backlight();
+        isLCDExist = true;
+    }
 
     uint8_t retryCounter = 0;
     struct tm timeinfo;
@@ -81,8 +99,17 @@ void loop()
 
     if (millis() - prevLogTime >= 10000)
     {
+        prevLogTime = millis();
+
         Serial.printf("RSSI: %d dBm\n", wifi.getRssi());
         WebSerial.printf("RSSI: %d dBm\n", wifi.getRssi());
-        prevLogTime = millis();
+
+        if (isLCDExist)
+        {
+            lcd.setCursor(0, 0);
+            lcd.print("=== BRIN  WATERQ-1 ===");
+            lcd.setCursor(0, 1);
+            lcd.printf("RSSI: %d dBm", wifi.getRssi());
+        }
     }
 }
